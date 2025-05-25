f392eb2587edd32df67839d6530a76528ac1e1b0
hanan zatar <90716074+hananzatar2001@users.noreply.github.com>
2025-05-25 08:09:26 +0300

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,20 +1,3 @@
-/*
-// playwright.config.js
-// playwright.config.js
-module.exports = {
-    projects: [
-        { name: 'chromium', use: { browserName: 'chromium' } },
-        { name: 'firefox', use: { browserName: 'firefox' } },
-    ],
-    use: {
-        headless: false,
-        viewport: { width: 1280, height: 720 },
-        baseURL: process.env.BASE_URL || 'https://www.saucedemo.com/',
-    },
-};
-*/
-
-// playwright.config.js
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
